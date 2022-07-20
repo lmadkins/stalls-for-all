6f9ce7bf395ac42fce76010a2c3adfbde0002fda
lmadkins <louise.m.adkins@gmail.com>
2022-07-20 11:17:45 -0700

    Added links and styles to About section and more to search container

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,51 @@ import React from 'react';
 function About() {
   return (
     <div className='aboutContainer'>
+      <p>
       This app is based on the principle that everyone has the right to safe and easy access to a restroom that meets their needs, and designed to help facilitate this.
         <br></br>
-     
+      The database of locations comes from <span> <a 
+        href='https://www.refugerestrooms.org/'
+        target='_blank'
+        rel="noreferrer"
+        aria-label='Link to Refuge Restroom Website'>
+        Refuge Restrooms
+      </a></span> 
+      , and utilizes their   
+      <span> <a 
+        href='https://www.refugerestrooms.org/api/docs/'
+        target='_blank'
+        rel="noreferrer"
+        aria-label='Link to Refuge Restroom API Website'>
+         API. 
+      </a></span>
+      <p></p>
+      For retrieving location and distance information, it utilizes <span><a 
+        href='https://www.geoapify.com/geocoding-api'
+        target='_blank'
+        rel="noreferrer"
+        aria-label='Link to Geoapify API Website'>
+        Geoapify's Geocoding API. 
+      </a></span> 
+      <p></p>
+      Site made using 
+      <span> <a 
+        href='https://reactjs.org/'
+        target='_blank'
+        rel="noreferrer"
+        aria-label='Link to React Website'>
+         React
+      </a> </span> 
+      and 
+      <span> <a 
+        href='https://react-bootstrap.netlify.app/'
+        target='_blank'
+        rel="noreferrer"
+        aria-label='Link to React Bootstrap Website'>
+        React Bootstrap. 
+      </a></span> 
+
+      </p>
     </div>
   );
 }
